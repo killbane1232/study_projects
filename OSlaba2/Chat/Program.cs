d2ad194b779f6da4824765a98e8d4900cbbfb17d
killbane1232 <gackconnor123@gmail.com>
2021-11-10 02:35:23 +0300

    added socket project

---
+++ OSlaba2/Chat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chat
+{
+    class Program
+    {
+        static int port = 8005;
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
