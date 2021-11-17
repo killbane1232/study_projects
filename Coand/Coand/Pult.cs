b1f22319f1b7c927f3900b365a5ef76b32bbca76
killbane1232 <gackconnor123@gmail.com>
2021-11-17 12:17:48 +0300

    repush

--- Coand/Coand/Pult.cs
+++ Coand/Coand/Pult.cs
@@ -1,16 +1,45 @@
+using System.Windows.Input;
 using System;
 namespace Coand
 {
     public class Pult
     {
+        ICommand[] onCommand;
+        ICommand[] offCommand;
         ICommand lastCommand;
-        public void Executer(ICommand command)
+        public Pult()
         {
-            command.Execute();
-            lastCommand = command;
-            Console.WriteLine(lastCommand.Name+":Executed");
+            var room1 = new Light("room1");
+            var room2 = new Light("room2");
+            var room3 = new Light("room3");
+            var room1On = new LightOnCommand(room1);
+            var room1Off = new LightOffCommand(room1);
+            var room2On = new LightOnCommand(room2);
+            var room2Off = new LightOffCommand(room2);
+            var room3On = new LightOnCommand(room3);
+            var room3Off = new LightOffCommand(room3);
+            onCommand = new ICommand[]
+            {
+                room1On,room2On,room3On
+            };
+            offCommand = new ICommand[]
+            {
+                room1Off,room2Off,room3Off
+            };
         }
-        public void UndoLast()
+        public void OnOnButtonClick(int id)
+        {
+            onCommand[id].Execute();
+            lastCommand = onCommand[id];
+            Console.WriteLine(lastCommand.Name+":Execute");
+        }
+        public void OnOffButtonClick(int id)
+        {
+            offCommand[id].Execute();
+            lastCommand = offCommand[id];
+            Console.WriteLine(lastCommand.Name+":Execute");
+        }
+        public void OnUndoButtonClick()
         {
             if(lastCommand!=null)
             {
--- Coand/Coand/main.cs
+++ Coand/Coand/main.cs
@@ -5,23 +5,14 @@ namespace Coand
         public static void Main()
         {
             var pult = new Pult();
-            var room1 = new Light("room1");
-            var room2 = new Light("room2");
-            var room3 = new Light("room3");
-            var room1On = new LightOnCommand(room1);
-            var room1Off = new LightOffCommand(room1);
-            var room2On = new LightOnCommand(room2);
-            var room2Off = new LightOffCommand(room2);
-            var room3On = new LightOnCommand(room3);
-            var room3Off = new LightOffCommand(room3);
-            pult.Executer(room1On);
-            pult.Executer(room1Off);
-            pult.Executer(room2On);
-            pult.Executer(room1On);
-            pult.Executer(room3On);
-            pult.UndoLast();
-            pult.Executer(room3On);
-            pult.Executer(room3Off);
+            pult.OnOnButtonClick(0);
+            pult.OnOffButtonClick(0);
+            pult.OnOnButtonClick(1);
+            pult.OnOnButtonClick(0);
+            pult.OnOnButtonClick(2);
+            pult.OnUndoButtonClick();
+            pult.OnOnButtonClick(2);
+            pult.OnOffButtonClick(2);
         }
     }
 }
