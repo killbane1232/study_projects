b036f627369aa09a9fa3ff1aa051f5b8b67d1f78
killbane1232 <gackconnor123@gmail.com>
2021-11-17 18:12:10 +0300

    OSLaba3 copleted

--- OSlaba3/laba3.sh
+++ OSlaba3/laba3.sh
@@ -1,8 +1,10 @@
 folders=$(ls -d */ | sort -g)
 rm result.txt
+k="\n"
 for i in $folders; do
     files=$(find $i -type f)
     for j in $files; do
         cat $j >> result.txt
+        echo >>result.txt
     done
 done
