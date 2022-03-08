5d23d637b5b9aa5e811ba3f45ed5b1b5c3a7b9e2
Bane <gackconnor123@gmail.com>
2022-03-08 20:17:37 +0300

    added oslaba31

---
+++ OSLaba31/OSLaba31/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace OSLaba31
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.progressPercent = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 88);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 23);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 0;
+            // 
+            // progressPercent
+            // 
+            this.progressPercent.AutoSize = true;
+            this.progressPercent.Location = new System.Drawing.Point(12, 72);
+            this.progressPercent.Name = "progressPercent";
+            this.progressPercent.Size = new System.Drawing.Size(33, 13);
+            this.progressPercent.TabIndex = 1;
+            this.progressPercent.Text = "100%";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 25);
+            this.trackBar1.Maximum = 10000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(189, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(215, 149);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.progressPercent);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label progressPercent;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ OSLaba31/OSLaba31/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OSLaba31
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool first = true;
+        private CancellationToken token = new CancellationToken();
+        CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+        public delegate void InvokeDelegate(object[] arr);
+
+        private void Tasker(int val)
+        {
+            var cnt = 20;
+            if (val < 20) cnt = val;
+            var list = new Task[cnt];
+
+            var results = new List<decimal>(); 
+            var i = 1;
+            for (; i <= cnt; i++)
+            {
+                int buf = i;
+                results.Add(1);
+                list[i-1]=Task.Factory.StartNew(() => Factor(buf,cnt,results, val));
+            }
+
+            i = 0;
+            while(i<cnt)
+            {
+                Task.WaitAny(list);
+                i = 0;
+                for(int j = 0;j<cnt;j++)
+                    if (list[j].IsCompleted)
+                        i++;
+                string r = (((i + 1) / cnt) * 100).ToString();
+                var arr2 = new object[1];
+                arr2[0] = r;
+                progressPercent.BeginInvoke(new InvokeDelegate(InvokeMethod), arr2);
+                //progressBar.Value = ((i + 1) / cnt) * 100;
+            }
+
+            decimal? res=1;
+            results.ForEach(x=>res*=x);
+            var arr = new object[1] ;
+            arr[0] = res.ToString() as object;
+            label2.BeginInvoke(new InvokeDelegate(InvokeLabel), arr);
+        }
+        public void InvokeMethod(object[] percent)
+        {
+            progressPercent.Text = percent[0] as string;
+            progressBar.Value = int.Parse(percent[0] as string);
+        }
+        public void InvokeLabel(object[] percent)
+        {
+            label2.Text = percent[0] as string;
+        }
+        private void Factor(int i, int cnt, List<decimal> results, int val)
+        {
+            for (var j = i; j < val; j += cnt)
+                results[i-1] *= j;
+        }
+
+        private void trackBar1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            label1.Text = trackBar1.Value.ToString();
+            if (trackBar1.Value != 0)
+            {
+                CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+                if (!first && !token.IsCancellationRequested)
+                    cancelTokenSource.Cancel();
+                while (mac)
+                    Thread.Sleep(100);
+                cancelTokenSource = new CancellationTokenSource();
+                token = cancelTokenSource.Token;
+                var val = trackBar1.Value;
+                Task.Factory.StartNew(() => Tasker(val), token);
+            }
+            else
+            {
+                progressPercent.Text = "100";
+                progressBar.Value = 100;
+                label2.Text = "1";
+            }
+        }
+    }
+}
