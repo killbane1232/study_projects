0a49deb481b87721657fd7c6d2f61414d594f1ef
Лебедев Яков Аркадьевич <y.lebedev@univ.priv>
2022-04-13 15:04:47 +0300

    skiplist

---
+++ SkipListForStudents/SkipList2020/SkipListLib.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SkipList2020
+{
+    public class SkipList<TKey, TValue> :
+        IEnumerable<KeyValuePair<TKey, TValue>>
+        where TKey : IComparable<TKey>
+    {
+        Node<TKey, TValue>[] _head;
+        readonly double _probability;
+        readonly int _maxLevel;
+        int _curLevel;
+        Random _rd;
+        public int Count { get; private set; }
+        public SkipList(int maxLevel = 10, double probability = 0.5)
+        {
+            _maxLevel = maxLevel;
+            _probability = probability;
+            _head = new Node<TKey, TValue>[_maxLevel];
+            for (int i = 0; i < maxLevel; i++)
+            {
+                _head[i] = new Node<TKey, TValue>();
+                if (i == 0) continue;
+                _head[i - 1].Up = _head[i];
+                _head[i].Down = _head[i - 1];
+            }
+
+            _curLevel = 0;
+            _rd = new Random(DateTime.Now.Millisecond);
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            var prevNode = new Node<TKey, TValue>[_maxLevel];
+            var currentNode = _head[_curLevel];
+            for (int i = _curLevel; i >= 0; i--)
+            {
+                while (currentNode.Right != null && currentNode.Right.Key.CompareTo(key) < 0)
+                {
+                    currentNode = currentNode.Right;
+                }
+                prevNode[i] = currentNode;
+                if (currentNode.Down == null)
+                    break;
+                currentNode = currentNode.Down;
+            }
+            int level = 0;
+            while (_rd.NextDouble() < _probability && level < _maxLevel - 1)
+            {
+                level++;
+            }
+            while (_curLevel < level)
+            {
+                _curLevel++;
+                prevNode[_curLevel] = _head[_curLevel];
+            }
+            for (int i = 0; i <= level; i++)
+            {
+                var node = new Node<TKey, TValue>(key, value) { Right = prevNode[i].Right };
+                prevNode[i].Right = node;
+                if (i == 0) continue;
+                node.Down = prevNode[i - 1].Right;
+                prevNode[i - 1].Right.Up = node;
+            }
+            Count++;
+        }
+
+        public bool Contains(TKey key)
+        {
+            var node = _head[_curLevel];
+            for (int i = _curLevel; i >= 0; i--)
+            {
+                if (node.Key.Equals(key))
+                    return true;
+                while (node.Right != null && node.Right.Key.CompareTo(key) <= 0)
+                {
+                    node = node.Right;
+                }
+                if (node.Down == null)
+                    break;
+                node = node.Down;
+            }
+            if (node.Key.Equals(key))
+                return true;
+            return false;
+        }
+
+        public void Remove(TKey key)
+        {
+            var prvNode = _head[_curLevel];
+            for (int i = _curLevel; i >= 0; i--)
+            {
+                while (prvNode.Right != null && prvNode.Right.Right !=null && prvNode.Right.Right.Key.CompareTo(key) <= 0)
+                {
+                    prvNode = prvNode.Right;
+                }
+                if (prvNode.Right!=null && prvNode.Right.Key.Equals(key))
+                    break;
+                if (prvNode.Down == null)
+                    return;
+                prvNode = prvNode.Down;
+            }
+            if (prvNode.Right==null || !prvNode.Right.Key.Equals(key))
+                return;
+            while(prvNode!=null)
+            {
+                if (prvNode.Right.Key.Equals(key))
+                    prvNode.Right = prvNode.Right.Right;
+                else 
+                {
+                    while(prvNode.Right !=null && !prvNode.Right.Key.Equals(key))
+                        prvNode = prvNode.Right;
+                    if(prvNode.Right != null)
+                        prvNode.Right = prvNode.Right.Right;
+                }
+                prvNode = prvNode.Down;
+            }
+            for(int i= _maxLevel-1; i>=0;i--)
+            {
+                if (_head[i].Right != null) 
+                { 
+                    _curLevel = i;
+                    break;
+                }
+
+            }
+            Count--;
+        }
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            for (var node = _head[0].Right; node.Right != null; node = node.Right)
+            {
+                yield return new KeyValuePair<TKey, TValue>(node.Key, node.Value);
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ SkipListForStudents/SkipListUnitTests/SkipListUnitTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SkipList2020;
+
+namespace SkipListUnitTests
+{
+    [TestClass]
+    public class SkipListUnitTest
+    {
+        [TestMethod]
+        public void CountIncreaseAfterAdding()
+        {
+            int n = 10;
+            var lib = new SkipList<int, int>();
+
+            for(int i=0; i<n; i++)
+            {
+                lib.Add(i, i);
+            }
+            Assert.AreEqual(n, lib.Count);
+        }
+        [TestMethod]
+        public void ItemsExistsAfterAdding()
+        {
+            var lib = new SkipList<int, int>();
+            var nums = new List<int>(new[] { 44, 22, 1 , 56, 3, 90, 31, 15, 26 });
+            int n = nums.Count;
+            for (int i = 0; i < n; i++)
+            {
+                lib.Add(nums[i], i);
+            }
+            nums.Sort();
+            int j = 0;
+            foreach(var pair in lib)
+            {
+                Assert.AreEqual(nums[j], pair.Key);
+                j++;
+            }
+            Assert.AreEqual(n, lib.Count);
+        }
+        [TestMethod]
+        public void ContainsTestAdding()
+        {
+            var lib = new SkipList<int, int>();
+            var nums = new List<int>(new[] { 44, 22, 1, 56, 3, 90, 31, 15, 26 });
+            int n = nums.Count;
+            for (int i = 0; i < n; i++)
+            {
+                lib.Add(nums[i], i);
+            }
+            nums.Sort();
+            for (int i = 0; i < n; i++)
+            {
+                Assert.IsTrue(lib.Contains(nums[i]));
+            }
+        }
+        [TestMethod]
+        public void RemoveTestAdding()
+        {
+            var lib = new SkipList<int, int>();
+            var nums = new List<int>(new[] { 44, 22, 1, 56, 3, 90, 31, 15, 26 });
+            int n = nums.Count;
+            for (int i = 0; i < n; i++)
+            {
+                lib.Add(nums[i], i);
+            }
+            nums.Sort();
+            for (int i = 0; i < n; i += 3)
+                lib.Remove(nums[i]);
+            for (int i = 0; i < n; i++)
+            {
+                if(i%3==0)
+                    Assert.IsFalse(lib.Contains(nums[i]));
+                else
+                    Assert.IsTrue(lib.Contains(nums[i]));
+            }
+        }
+        [TestMethod]
+        public void RandomItemsExistsAfterAdding()
+        {
+            var lib = new SkipList<int, int>();
+            var nums = new HashSet<int>();
+            var rd = new Random();
+            int n = 100;
+            while (nums.Count < n)
+            {
+                nums.Add(rd.Next(1, n * 3));
+            }
+            foreach(var item in nums)
+            {
+                lib.Add(item,1);
+            }
+
+            var a = nums.ToList();
+            a.Sort();
+            int j = 0;
+            foreach (var pair in lib)
+            {
+                Assert.AreEqual(a[j], pair.Key);
+                j++;
+            }
+            Assert.AreEqual(n, lib.Count);
+        }
+    }
+}
